03230301d64f7a2885fe09fe0da293c5f2eaf3e8
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-14 13:48:43 -0500

    Get by id for projects

--- api/server.js
+++ api/server.js
@@ -23,6 +23,59 @@ server.get('/api/projects', (req,res) => {
             })
 })
 
+server.get('/api/projects/:id', (req,res) => {
+    const { id } = req.params;
+    console.log(id)
+    try {        
+        if(id) {
+            Projects.get(id)
+                .then(proj => {
+                    res.status(200).json({
+                        id: proj.id,
+                        name: proj.name,
+                        description: proj.description,
+                        completed: proj.completed
+                    })
+                })
+                .catch(err => {
+                    res.status(404).json({
+                        message: "Id does not exist",
+                        err: err.message
+                    })
+            })
+        }
+        else {
+            res.status(404).json({
+                message: "Invalid Id"
+            })
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            message: "Our Bad... Something on our end is messed up",
+            err: err.message,
+            stack: err.stack 
+        })
+    }
+})
+
+// server.post('/api/projects', (req,res) => {
+//     try{
+//         Projects.insert()
+//             .then(results => {
+//                 console.log(results)
+//             })
+//             .catch(err => console.log(err))
+//     }
+//     catch(err){
+//         res.status(500).json({
+//             message: "Our Bad... Something on our end is messed up",
+//             err: err.message,
+//             stack: err.stack 
+//         })
+//     }
+// })
+
 server.use('/', (req,res) => {
     res.status(404).json({
         message: "Not Found"
