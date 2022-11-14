beb7c0e347704000cdabc331b4ff25e18b865d5f
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-14 13:17:43 -0500

    get projects working

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,30 @@
 // Write your "actions" router here!
+// const express = require('express');
+// const { /** Middleware for actions */} = require('./actions-middlware')
+// const router = express.Router();
+
+
+
+
+
+// router.get('/api/actions', (req,res) => {
+//     console.log(`get`)
+// })
+
+// router.get('/api/actions/:id', (req,res) => {
+//     console.log(`get with id`)
+// })
+
+// router.post('/api/actions', (req,res) => {
+//     console.log(`post`)
+// })
+
+// router.put('/api/actions/:id', (req,res) => {
+//     console.log(`put`)
+// })
+
+// router.delete('/api/actions/:id', (req,res) => {
+//     console.log(`delete`)
+// })
+
+// module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const server = express();
+server.use(express.json())
+
+const Projects = require('./projects/projects-model');
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+server.get('/api/projects', (req,res) => {
+    Projects.get()
+            .then(project => {
+                res.json(project)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "Our Bad... Something on our end is messed up",
+                    err: err.message,
+                    stack: err.stack 
+                })
+            })
+})
+
+server.use('/', (req,res) => {
+    res.status(404).json({
+        message: "Not Found"
+    })
+})
+
 module.exports = server;
+
+// {throw new Error(`NOOOOOO`)}
